09007d09e34372a2918f66948f8df3b28c4998e0
plok6325 <zhanghongbingod@outlook.com>
2016-10-25 21:31:48 +0800

    display image from web

--- main.py
+++ main.py
@@ -6,12 +6,30 @@ Created on Tue Oct 25 18:13:55 2016
 """
 
 from Tkinter import *
+from PIL import ImageTk, Image
+import os
+import urllib
 
+def get_image():
+    file = StringIO(urllib.urlopen('https://lh4.googleusercontent.com/-v0soe-ievYE/AAAAAAAAAAI/AAAAAAADnx8/TYw5hefoVmg/s0-c-k-no-ns/photo.jpg').read())
+    img1 = ImageTk.PhotoImage(Image.open(file))
+    return img1
+    
+    
+    
+    
+    
+try:
+    from cStringIO import StringIO #python 2
+except ImportError:
+    from io import StringIO #python 3
 root = Tk()
 
-var = StringVar()
-label = Label( root, textvariable=var, relief=RAISED,image=)
+img1=get_image()
 
-var.set("Hey!? How are you doing?")
-label.pack()
+
+panel = Label(root, image = img1)
+
+panel.pack(side = "bottom", fill = "both", expand = "yes")
+
 root.mainloop()
