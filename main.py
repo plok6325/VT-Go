d406d3dabc72c03789f5de2e2069830ca8bd5b20
plok6325 <zhanghongbingod@outlook.com>
2016-10-26 22:12:29 +0800

    demo
    tested on win 10 py 2.7

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ import os
 import urllib 
 import re
 import random  
+import pandas as pd 
 
 def get_image(match):
     idx=random.randint(0, len(match)-1)
@@ -18,7 +19,7 @@ def get_image(match):
     match.pop(idx)                
     file = StringIO(urllib.urlopen('https://github.com/plok6325/VT-Go/raw/'+image_url).read())
     img1 = ImageTk.PhotoImage(Image.open(file))
-    return img1
+    return img1,image_url
     
    
 def get_image_url():
@@ -37,25 +38,42 @@ flag=0
 
 
 def real():
-    top.destroy()
+    global imgname
+    numr=result_tuple[imgname][1]
+    nump=result_tuple[imgname][0]
+    result_tuple[imgname]=(numr+1,nump)
+    result[imgname]=0
+    change_image()
     
 def pc():
-    top.destroy()
+    global imgname
+    numr=result_tuple[imgname][1]
+    nump=result_tuple[imgname][0]
+    result_tuple[imgname]=(numr,nump+1)
+    result[imgname]=1
+    change_image()
     
 def submit():
+    global det 
     root.destroy()
+    det=1
     
 def change_image():
-    imgt=get_image(image_url)
+    global imgname
+    imgt,imgname=get_image(image_url)
+    result_tuple[imgname]=(0,0)
     panel.config(image=imgt)
     panel.image = imgt
-
+    
     
 if flag==0:
     image_url=(get_image_url())
     flag=1
     
-    
+result_tuple={}
+result={}
+
+
 root = Tkinter.Tk()
 root.withdraw()
 
@@ -65,20 +83,25 @@ imageFrame.pack(side=Tkinter.TOP)
 bottonFrame=Tkinter.Frame(toplvl)
 bottonFrame.pack(side=Tkinter.BOTTOM)
 
-img1=get_image(image_url)
+img1,imgname=get_image(image_url)
+result_tuple[imgname]=(0,0)
+
 panel = Tkinter.Label(imageFrame, image = img1)
 panel.pack(side = "top", fill = "both", expand = "yes")
-    
-realbutton = Tkinter.Button(bottonFrame, text="   R  E  A  L   ", command = change_image )
+realbutton = Tkinter.Button(bottonFrame, text="   R  E  A  L   ", command = real)
 realbutton.pack( side = Tkinter.LEFT)
-
-pcbutton = Tkinter.Button(bottonFrame, text="      P    C    ", command = change_image)
+pcbutton = Tkinter.Button(bottonFrame, text="      P    C    ", command = pc)
 pcbutton.pack( side = Tkinter.RIGHT )
 subbutton = Tkinter.Button(bottonFrame, text="   s u b m i t    ", command = submit)
 subbutton.pack( side = Tkinter.LEFT )
     
-
 root.mainloop()  
+
+
+if det==1:
+    df=pd.Series(result)
+    df.to_csv('result.csv')
+    df=pd.Series(result)
 
 
     
