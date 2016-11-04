7b345510dc8e421f1a1f2f0b6e1f231faebab989
plok6325 <zhanghongbingod@outlook.com>
2016-11-04 23:54:37 +0800

    完成更新runm

--- runme.m
+++ runme.m
@@ -1,8 +1,14 @@
 %% run me
-run_me_version='1.0.0'
+current_run_me_version='1.0.0'
+
+
 page=urlread('https://github.com/plok6325/VT-Go');
 image_V=regexp(page,'imgpatch\d\.\d\.\d\.zip');
-
+run_me_version=page(regexp(page,'runme_version')+16:regexp(page,'runme_version')+20);
+if strcmp(current_run_me_version,run_me_version)
+else
+    websave('runme.m','https://github.com/plok6325/VT-Go/raw/master/runme.m');
+end
 main_version=page(regexp(page,'script_version')+17:regexp(page,'script_version')+21);
 image_version=page(image_V(1)+8:image_V(1)+12);
 
