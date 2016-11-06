54745a05a28049f35180b7fb799baeac59cc1b5c
plok6325 <zhanghongbingod@outlook.com>
2016-11-06 21:19:32 +0800

    刚才忘改readme

--- README.md
+++ README.md
@@ -5,4 +5,4 @@ BICV 1598
 ## 
 runme_version # 1.0.0
 
-script_version # 1.2.0
+script_version # 1.3.0
--- runme.m
+++ runme.m
@@ -16,11 +16,11 @@ image_version=page(image_V(1)+8:image_V(1)+12);
 if strcmp(currentimageversion,image_version) && strcmp(main_version,currentmainversion)
     main(image_version,1);
 else
+    rmdir(['images',currentimageversion],'s')
     display('new version found, updating ')
     zipname=['imgpatch',image_version,'.zip'];
     websave(zipname,['https://github.com/plok6325/VT-Go/raw/master/',zipname]);
     unzip(zipname,['.\images',image_version]);
-    rmdir(['images',currentimageversion],'s')
     websave('main.m','https://github.com/plok6325/VT-Go/raw/master/main.m');
     display('done run script again');
 end
