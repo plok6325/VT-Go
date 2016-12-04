03b9a42927c326c2018a56a5127f38e11f6e7b1c
plok6325 <zhanghongbingod@outlook.com>
2016-12-05 07:41:39 +0800

    base

--- version_check.m
+++ version_check.m
@@ -2,15 +2,13 @@ function [currentmainversion ,currentimageversion]  = version_check(  )
 %UNTITLED3 此处显示有关此函数的摘要
 %   此处显示详细说明
 
-imageversion=dir('*images*');
-if length(imageversion)~=1
-    mkdir images1.1.0\
-    currentimageversion='1.1.0'
-    currentmainversion=main(1,0);
-else
-currentimageversion=imageversion(1).name(7:11);
-currentmainversion=main(1,0,0);main( 1, 0,0,0, 0, 0,0)
+
+imageversion=textread('./image_database/version.txt', '%s', 'whitespace', '');
+    imageversion=imageversion{1};
+currentimageversion=imageversion(end-4:end);
+currentmainversion=main(1,0,0);
+
 version={currentmainversion ,currentimageversion} ;
 end
-end
+
 
